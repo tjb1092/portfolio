86ccbc214ec87f63dca8a881a88fb8ac54f2fc9f
Tony Bailey <baileytj@mail.uc.edu>
2017-09-13 14:35:20 -0400

    Moving to a repo on github so I can work on multiple computers

---
+++ js/port.js
@@ -0,0 +1,8 @@
+function showModal(){
+    document.getElementById("mainModal").style.display = "block";
+}
+
+function closeModal(){
+    document.getElementById("mainModal").style.display = "none";
+}
+
